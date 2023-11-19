69b5a5f560309315065e245510ad4beff61beb5f
mrkeshi <mr.alireza.keshavarz@gmail.com>
2023-11-19 15:35:19 +0330

    update

--- Exercise91.c
+++ Exercise91.c
@@ -10,12 +10,14 @@ printf("\n please enter your number:");
 scanf("%d",&head);
 if (head==0) break;
 num=head;
+
 do
 {
     r=num%10;
     sum+=r;
     num=num/10;
 } while (num>0);
+
 printf("\n number: %d \n",head);
 printf("\n Sum of digits: %d \n",sum);
 }
